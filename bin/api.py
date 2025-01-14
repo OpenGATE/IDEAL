3930c46dec9161e57417ec34d52c3c95557b1ffa
Martina Favaretto <MartyDvrk>
2025-01-14 10:56:19 +0100

    started to log api info, to be completed

--- bin/api.py
+++ bin/api.py
@@ -15,35 +15,52 @@ import jwt
 import ideal_module as idm
 import utils.condor_utils as cndr 
 import utils.api_utils as ap 
-import impl.dicom_functions as dcm
+import impl.dual_logging as ideal_logging
 # api imports
+import logging
 from flask import Flask, request, jsonify, Response 
+from flask.logging import default_handler
 from flask_sqlalchemy import SQLAlchemy
 from apiflask import APIFlask, HTTPTokenAuth, abort
 from werkzeug.utils import secure_filename
 from werkzeug.security import check_password_hash, generate_password_hash
-from utils.api_schemas import SimulationRequest, Authentication, define_user_model
+import utils.api_schemas as api_s 
 
-# Initialize sytem configuration once for all
-sysconfig = idm.initialize_sysconfig(username = 'myqaion')
-base_dir = sysconfig['IDEAL home']
-input_dir = sysconfig["input dicom"]
-log_dir = sysconfig['logging']
+# get api configuration file
+this_cmd = os.path.realpath(__file__) 
+bin_dir = os.path.dirname(this_cmd)
+base_dir = os.path.dirname(bin_dir)
 daemon_cfg = os.path.join(base_dir,'cfg/log_daemon.cfg')
+sysconfig_path = os.path.join(base_dir,'cfg/system.cfg')
 log_parser = configparser.ConfigParser()
 log_parser.read(daemon_cfg)
 ideal_history_cfg = log_parser['Paths']['cfg_log_file']
 api_cfg = ap.get_api_cfg(log_parser['Paths']['api_cfg'])
-commissioning_dir = sysconfig['commissioning']
 
+# set up logger
+api_logfile_path = api_cfg['logging']['logfile']
+log_severity = api_cfg['logging']['severity']
+
+ideal_logging.configure_api_logging(api_logfile_path,level = log_severity)
+
+# create application
 app = APIFlask(__name__,title='IDEAL interface', version='1.0')
 auth = HTTPTokenAuth(scheme='Bearer')
+api_log = logging.getLogger()
+
+# Initialize sytem configuration once for all
+sysconfig = idm.initialize_sysconfig(username = 'myqaion')
+
+input_dir = sysconfig["input dicom"]
+log_dir = sysconfig['logging']
+commissioning_dir = sysconfig['commissioning']
 
 # api configuration
+host_IP = api_cfg['server']['IP host']
+api_log.info(f"API database: {'sqlite:///' + api_cfg['server']['credentials db']}")
 app.config['SECRET_KEY'] = os.urandom(24)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + api_cfg['server']['credentials db']# os.path.join(base_dir, 'database.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-host_IP = api_cfg['server']['IP host']
 
 # List of all active jobs. Members will be simulation objects
 max_queue_size = 50
@@ -53,7 +70,8 @@ queue = ap.preload_status_overview(ideal_history_cfg,max_size=max_queue_size)
 # register database 
 db = SQLAlchemy(app)
 
-User = define_user_model(db)  
+User = api_s.define_user_model(db)  
+Server = api_s.define_server_credentials_model(db)
 
 @auth.verify_token
 def verify_tocken(token): 
@@ -72,10 +90,13 @@ def verify_tocken(token):
  
 
 @app.route("/v1/auth", methods=['POST'])
-@app.input(Authentication,location = 'headers')
-def authentication(auth):
-    username = auth.get('account_login')
-    pwd = auth.get('account_pwd')
+def authentication():
+    username = request.headers.get('account_login')
+    pwd = request.headers.get('account_pwd')
+    if not username:
+        abort(400, message="Missing required header: 'Account-Login")
+    if not pwd:
+        abort(400, message="Missing required header: Account-Pwd")
     user = User.query.filter_by(username=username).first()
     if not user:
         return abort(401, message='Could not verify user!', detail={'WWW-Authenticate': 'Basic-realm= "No user found!"'})  
@@ -92,12 +113,13 @@ def version():
 
 @app.post("/v1/jobs")
 @app.auth_required(auth)
-@app.input(SimulationRequest, location='form_and_files')
-def start_new_job(data):  
+def start_new_job():  
     # clean input directory
     ap.remove_directory_contents(input_dir)
     
     # get data from client
+    data = api_s.SimulationRequest().load({**request.form, **request.files})
+
     rp_file = data['dicomRtPlan']
     rp_filename = secure_filename(rp_file.filename)
     rs_file = data['dicomStructureSet']
@@ -127,7 +149,8 @@ def start_new_job(data):
     if 'phantom' in data:
         phantom = data['phantom']
     
-    data_checksum = ap.sha1_directory_checksum(commissioning_dir)
+    data_checksum = ap.sha1_directory_checksum(commissioning_dir,sysconfig_path)
+
     if data_checksum != ref_checksum:
         return jsonify({"configChecksum":"Configuration has changed from frozen original one"}), 503
     
@@ -144,46 +167,47 @@ def start_new_job(data):
     rd_file.save(os.path.join(datadir,secure_filename(rd_file.filename)))
     rds = ap.unzip_file(datadir,rd_file.filename)
     
+    # init simulation object
+    dicom_file = os.path.join(datadir,rp[0])
+    mc_simulation = idm.ideal_simulation(arg_username,dicom_file,n_particles = arg_number_of_primaries_per_beam,
+                                         uncertainty=arg_percent_uncertainty_goal, phantom = phantom)
+    
     # check dicom
-    ok, missing_keys = dcm.verify_all_dcm_keys(datadir,rp,rs,cts,rds)
+    ok, missing_keys = mc_simulation.verify_dicom_input_files()
     if not ok:
         #return Response(str(missing_keys), status=422, mimetype='application/json')
         return jsonify(missing_keys), 422
     
+    api_log.info('All DICOM files read in correctly and conform to IDEAL standards')
+    
     # create simulation object
-    dicom_file = os.path.join(datadir,rp[0])
     try:
-        mc_simulation = idm.ideal_simulation(arg_username,dicom_file,n_particles = arg_number_of_primaries_per_beam,
-                                             uncertainty=arg_percent_uncertainty_goal, phantom = phantom)
+        mc_simulation.read_in_data(verify=False)
     except Exception as e:
         abort(500, message=str(e))
         
-    # # check dicom files
-    # ok, missing_keys = mc_simulation.verify_dicom_input_files()
-    
     # Get job UID
     jobID = mc_simulation.jobId
+    api_log.info(f'Created simulation object with {jobID = }')
     
     # start simulation and append to list 
+    api_log.info('Trying to create working directory and dispatch the simulations to HTCondor')
     try:
         mc_simulation.start_simulation()
     except Exception as e:
+        api_log.error(e)
         abort(500, message=str(e))
         
+    api_log.info('Simulations submitted successfully')
     # remove oldest job if the queue has reached max size
     if len(jobs_list) >= max_queue_size:
+        api_log.warning(f'Reached max number of tracked simulations ({max_queue_size}). Only the latest {max_queue_size} will be available from this API. Older simulations can still be monitored from {ideal_history_cfg}')
         print('POP FIRST!')
         first_job = next(iter(jobs_list))
         jobs_list.pop(first_job)
         
-    jobs_list[jobID] = mc_simulation
-    
-    # check stopping criteria:
-    try:
-        mc_simulation.start_job_control_daemon()
-    except Exception as e:
-        abort(500, message=str(e))
-        
+    jobs_list[jobID] = mc_simulation   
+    api_log.info(f'Simulation with {jobID = } added to the jobs list')
         
     return Response(jobID, status=201, mimetype='text/plain')
     
@@ -257,7 +281,10 @@ def stop_job(jobId):
             return Response('Job not finished yet', status=409, mimetype='text/plain')
         
         outputdir = jobs_list[jobId].outputdir
-        r = ap.transfer_files_to_server(outputdir,api_cfg)
+        username = 'admin'
+        server = Server.query.filter_by(username=username).first()
+        login_data = {'account-login': server.username_b64, 'account-pwd': server.password}
+        r = ap.transfer_files_to_server(outputdir,api_cfg,login_data)
         if r.status_code == 200:
             return Response('The results will be sent', status=200, mimetype='text/plain')
         else:
@@ -278,8 +305,10 @@ def get_status(jobId):
 
 if __name__ == '__main__':
     
-    context = ('/etc/ssl/certs/cert_ideal_final.pem','/home/montecarlo/key_ideal_final.pem')
-    app.run(host=host_IP,port=5000,ssl_context=context)
+    cert = api_cfg['server']['ssl cert']
+    key = api_cfg['server']['ssl key']
+    context = (cert, key)
+    app.run(host=host_IP,port=5000)#,ssl_context=context)
     
 
     
