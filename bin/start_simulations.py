0bbae54c27af9e5889f581ffb90eba6b20b4f847
Martina Favaretto <MartyDvrk>
2025-04-14 16:25:06 +0200

    RBE table in the config only if we are calculating RBE

--- bin/start_simulations.py
+++ bin/start_simulations.py
@@ -45,7 +45,7 @@ def run_sim_single_beam(rungate_workdir, cfg_data_obj, beam_name,n_particles = 0
             F_clin = float(cfg_data['f_clin'])
         if rbe_model == 'LEM1lda':
             D_cut = float(cfg_data['d_cut'])
-    lookup_table_filename = cfg_data['rbe table']
+        lookup_table_filename = cfg_data['rbe table']
 
     if not output_path :
         output_path = '/opt/share/IDEAL-1_2ref/'
