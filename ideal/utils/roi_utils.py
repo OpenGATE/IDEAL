48fe4dfbc233bc59e1c18ee1440f1d3bf6610b67
Martina Favaretto <MartyDvrk>
2025-06-06 15:45:10 +0200

    fixed bug with z_step. The fix was present in v1.1, but was not included in v2 until now.

--- ideal/utils/roi_utils.py
+++ ideal/utils/roi_utils.py
@@ -358,6 +358,7 @@ class region_of_interest(object):
         self.contour_layers=[]
         self.zlist = []
         self.dz = 0.
+        self.z_precision = 3
         self.maskparameters = []
         self.masklist = []
         #self.contour_refs=[]
@@ -370,7 +371,7 @@ class region_of_interest(object):
             zvalues = set(points[:,2])
             # check assumption that all points are in the same xy plane (constant z)
             assert(len(zvalues)==1)
-            zvalue = zvalues.pop()
+            zvalue = round(zvalues.pop(), self.z_precision)
             if zvalue in self.zlist:
                 ic = self.zlist.index(zvalue)
                 self.contour_layers[ic].add_contour(points,ref)
@@ -387,7 +388,7 @@ class region_of_interest(object):
         if len(dz) == 1:
             self.dz = dz.pop()
         else:
-            dz = set(np.diff(np.around(self.zlist,decimals=6)))
+            dz = set(np.around(np.diff(self.zlist),decimals=self.z_precision))
             if len(dz) == 1:
                 self.dz = dz.pop()
             else:
@@ -412,7 +413,7 @@ class region_of_interest(object):
         self.maskparameters = []
         self.masklist = []
         for contour_layer in contours_list:
-            z = contour_layer.z
+            z = round(contour_layer.z, self.z_precision)
             self.zlist.append(z)
             self.contour_layers.append(contour_layer)
         zlist = sorted(self.zlist)
@@ -420,7 +421,7 @@ class region_of_interest(object):
         if len(dz) == 1:
             self.dz = dz.pop()
         else:
-            dz = set(np.diff(np.around(zlist,decimals=6)))
+            dz = set(np.around(np.diff(self.zlist),decimals=self.z_precision))
             if len(dz) == 1:
                 self.dz = dz.pop()
             else:
@@ -495,7 +496,7 @@ class region_of_interest(object):
         #logger.debug("got point mesh")
         if zmin-eps>self.bb.zmax+self.dz or zmax+eps<self.bb.zmin-self.dz:
             logger.warn("WARNING: no overlap in z ranges")
-            logger.warn("WARNING: img z range [{}-{}], roi z range [{}-{}]".format(zmin,zmax,self.zmin,self.zmax))
+            logger.warn("WARNING: img z range [{}-{}], roi z range [{}-{}]".format(zmin,zmax,self.bb.zmin,self.bb.zmax))
             return roimask
         img_params = [orig, space, dims, zrange]
         if zrange is None:
@@ -714,6 +715,5 @@ def intersect_segments(S1, S2, eps = 1e-10):
     if(tI < 0 or tI > 1):
         return np.array([])
     return(S1[0] + sI * u)
-
 
 # vim: set et softtabstop=4 sw=4 smartindent:
